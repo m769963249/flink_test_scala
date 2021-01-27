5522b4cd145b02ffc95373da238fba7ea0833274
mawenbin <lvaczuhc@outlook.com>
2021-01-27 18:53:59 +0800

    chang word content

--- src/main/scala/org/example/BatchJob.scala
+++ src/main/scala/org/example/BatchJob.scala
@@ -7,7 +7,7 @@ import org.apache.flink.streaming.api.scala._
 object BatchJob {
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
-    val source = env.fromElements("java spark flink python hive java spark flink")
+    val source = env.fromElements("Thank you\nAre you OK\nHello\nThank you\nThank you very much\nHello\nThank you\nHow are you Indian Mi fans\nDo you like Mi 4i")
     val value = source.flatMap(_.split(" ")).map((_, 1)).groupBy(0).sum(1)
     value.print()
 
